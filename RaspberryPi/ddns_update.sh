39260569d0bc539cd0d50d92c0041acb20575b82
Yi-Hua Liang <yihua1218@gmail.com>
2017-12-13 05:08:39 +0000

    Only match one lilne

--- RaspberryPi/ddns_update.sh
+++ RaspberryPi/ddns_update.sh
@@ -4,14 +4,15 @@ source /etc/ddns/he.net.conf
 
 LAST_DDNS_UPDATE_IP_FILE=/tmp/last_ddns_update_ip.log
 
-CURRENT_IPV6_IP_ADDRESS=`ip -6 addr show dev eth0 | grep "scope global noprefixroute" | awk 'BEGIN { FS = " " } { print $2 }' | awk 'BEGIN { FS = "/" } { print $1 }' | grep 2001:`
+CURRENT_IPV6_IP_ADDRESS=`ip -6 addr show dev eth0 | grep "scope global noprefixroute" | awk 'BEGIN { FS = " " } { print $2 }' | awk 'BEGIN { FS = "/" } { print $1 }' | grep -m 1 2001:`
 LAST_DDNS_UPDATE_IP_ADDRESS=`cat $LAST_DDNS_UPDATE_IP_FILE`
 
 if [ "$CURRENT_IPV6_IP_ADDRESS" == "$LAST_DDNS_UPDATE_IP_ADDRESS" ] ; then
   echo "Same LAN IPv6 IP Address: $LAST_DDNS_UPDATE_IP_ADDRESS"
 else
   echo "LAN IPv6 IP Address changed, update to dns.he.net"
-  curl "https://dyn.dns.he.net/nic/update?hostname=$DDNS_HOSTNAME&password=$DDNS_PASSWORD&myip=$CURRENT_IPV6_IP_ADDRESS"
+  echo curl "https://dyn.dns.he.net/nic/update?hostname=$DDNS_HOSTNAME&password=$DDNS_PASSWORD&myip=$CURRENT_IPV6_IP_ADDRESS"
+  curl \"https://dyn.dns.he.net/nic/update?hostname=$DDNS_HOSTNAME&password=$DDNS_PASSWORD&myip=$CURRENT_IPV6_IP_ADDRESS\"
   RESULT=$?
   if [ $RESULT -eq 0 ] ; then
     echo $CURRENT_IPV6_IP_ADDRESS > $LAST_DDNS_UPDATE_IP_FILE
