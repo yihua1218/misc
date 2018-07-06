53a609453a333336f39a07d2f9f0ef1095921c27
Yi-Hua Liang <yihua1218@gmail.com>
2018-07-06 18:36:05 +0800

    Updated scripts.

--- AWS/bin/autoscalingToZero.sh
+++ AWS/bin/autoscalingToZero.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-FILTER=test2
+if [ "$1" == "" ] ; then
+  BASENAME=`basename $0`
+  echo "Usage: $BASENAME <FILTER>"
+  exit 1
+fi
+
+FILTER=$1
 DATE=`date +%Y%m%d`
 BASE_DIR=~/data/nuclias/1.0
 AUTOSCALING_GROUP_NAME_FILE=$BASE_DIR/AutoScalingGroupName-$DATE.json
+
+if [ ! -d $BASE_DIR ]; then
+  mkdir -p $BASE_DIR
+fi
 
 if [ ! -e $AUTOSCALING_GROUP_NAME_FILE ]; then
   aws --profile nuclias --region ap-northeast-1 autoscaling describe-auto-scaling-groups > $AUTOSCALING_GROUP_NAME_FILE
@@ -13,7 +23,6 @@ TARGET_GROUP_NAMES=`cat $AUTOSCALING_GROUP_NAME_FILE | jq '.AutoScalingGroups[].
 
 for group_name in $TARGET_GROUP_NAMES; do
   name=`echo $group_name | sed 's/"//g'`
-  echo aws --profile nuclias --region ap-northeast-1 autoscaling update-auto-scaling-group --auto-scaling-group-name $name --max-size 0
   aws --profile nuclias --region ap-northeast-1 autoscaling update-auto-scaling-group --auto-scaling-group-name $name --max-size 0
 done
 
--- AWS/bin/stopRds.sh
+++ AWS/bin/stopRds.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-FILTER=test2
+if [ "$1" == "" ] ; then
+  BASENAME=`basename $0`
+  echo "Usage: $BASENAME <FILTER>"
+  exit 1
+fi
+
+FILTER=$1
 DATE=`date +%Y%m%d`
 BASE_DIR=~/data/nuclias/1.0
 DB_INSTANCES_FILE=$BASE_DIR/describe-db-instances-$DATE.json
+
+if [ ! -d $BASE_DIR ]; then
+  mkdir -p $BASE_DIR
+fi
 
 if [ ! -e $DB_INSTANCES_FILE ]; then
   aws --profile nuclias --region ap-northeast-1 rds describe-db-instances > $DB_INSTANCES_FILE
